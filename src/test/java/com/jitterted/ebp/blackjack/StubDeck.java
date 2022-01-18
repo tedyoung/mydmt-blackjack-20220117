fcc7116b012670e23ce6c70d16ffc8af96f30628
Ted M. Young <tedyoung@gmail.com>
2022-01-18 12:58:43 -0800

    LAB #4: Game takes in Deck via constructor. First test of Player beats dealer passes.

---
+++ src/test/java/com/jitterted/ebp/blackjack/StubDeck.java
@@ -0,0 +1,28 @@
+package com.jitterted.ebp.blackjack;
+
+import com.jitterted.ebp.blackjack.domain.Card;
+import com.jitterted.ebp.blackjack.domain.Deck;
+import com.jitterted.ebp.blackjack.domain.Rank;
+import com.jitterted.ebp.blackjack.domain.Suit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class StubDeck extends Deck {
+    private static final Suit DUMMY_SUIT = Suit.HEARTS;
+    private final ListIterator<Card> iterator;
+
+    public StubDeck(Rank... ranks) {
+        List<Card> cards = new ArrayList<>();
+        for (Rank rank : ranks) {
+            cards.add(new Card(DUMMY_SUIT, rank));
+        }
+        this.iterator = cards.listIterator();
+    }
+
+    @Override
+    public Card draw() {
+        return iterator.next();
+    }
+}
---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -0,0 +1,37 @@
+package com.jitterted.ebp.blackjack.domain;
+
+import com.jitterted.ebp.blackjack.StubDeck;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameOutcomeTest {
+
+    @Test
+    public void playerBeatsDealer() {
+        Deck stubDeck = new StubDeck(Rank.TEN,   Rank.EIGHT,
+                                     Rank.QUEEN, Rank.JACK);
+        Game game = new Game(stubDeck);
+        game.initialDeal();
+
+        game.playerStands(); // make sure the player stands
+        game.dealerTurn(); // dealer needs to take its turn
+
+        String outcome = game.determineOutcome();
+        assertThat(outcome)
+                .isEqualTo("You beat the Dealer! 💵");
+    }
+
+
+//    @Test
+//    public void playerHitsAndGoesBustAndLoses() throws Exception {
+//        Game game = new Game();
+//        game.initialDeal();
+//
+//        game.playerHits();
+//
+//        assertThat(game.determineOutcome())
+//                .isEqualTo("You Busted, so you lose.  💸");
+//    }
+
+}
