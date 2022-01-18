f2be35f45cd2bc41f093147ea89ebbaca4a95397
Ted M. Young <tedyoung@gmail.com>
2022-01-18 10:16:52 -0800

    LAB #2: Extracted cardsAsString to its own method and wrote characterization test for it.

--- src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -15,4 +15,13 @@ class HandDisplayTest {
         assertThat(ConsoleHand.displayFirstCard(hand))
                 .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
     }
+
+    @Test
+    public void cardsAsStringTransformsWholeHandToDisplayString() throws Exception {
+        Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.NINE),
+                                     new Card(Suit.SPADES, Rank.THREE)));
+
+        assertThat(hand.cardsAsString())
+                .isEqualTo("[31m┌─────────┐[1B[11D│9        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        9│[1B[11D└─────────┘[6A[1C[30m┌─────────┐[1B[11D│3        │[1B[11D│         │[1B[11D│    ♠    │[1B[11D│         │[1B[11D│        3│[1B[11D└─────────┘");
+    }
 }
