2db5cc83fae88674dd63571128ca4cfca8c2fad2
Ted M. Young <tedyoung@gmail.com>
2022-01-19 11:54:19 -0800

    TDD: POST to /start-game does initial deal (and Spring configuration is happy with the @Bean "bridge" method)

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -1,13 +1,24 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlackjackController {
 
+    private final Game game;
+
+    @Autowired
+    public BlackjackController(Game game) {
+        this.game = game;
+    }
+
     @PostMapping("/start-game")
     public String startGame() {
+        // gameService.currentGame().initialDeal()
+        game.initialDeal();
         return "redirect:/";
     }
 
