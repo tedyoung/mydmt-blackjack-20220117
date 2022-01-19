b477fe4045fc3365492b131283799de311a50414
Ted M. Young <tedyoung@gmail.com>
2022-01-19 12:26:57 -0800

    TDD: POST to /hit endpoint redirects to /game and player hits (gets additional card)

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -33,7 +33,8 @@ public class BlackjackController {
 
     @PostMapping("/hit")
     public String hitCommand() {
-        return "redirect:/";
+        game.playerHits();
+        return "redirect:/game";
     }
 
 }
--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -51,4 +51,19 @@ class BlackjackControllerTest {
                 .containsExactly("10♦", "K♦");
     }
 
+    @Test
+    public void hitCommandResultsInPlayerHavingThreeCards() throws Exception {
+        Game game = new Game(new Deck());
+        BlackjackController blackjackController = new BlackjackController(game);
+        blackjackController.startGame();
+
+        String redirectPage = blackjackController.hitCommand();
+
+        assertThat(redirectPage)
+                .isEqualTo("redirect:/game");
+
+        assertThat(game.playerHand().cards())
+                .hasSize(3);
+    }
+
 }
